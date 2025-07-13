6714e3e680d0d3bfdf888112be16bebc38aa87e8
mina bergova <bergovam@gmail.com>
2025-07-13 07:56:24 +0300

    Add body parser middleware

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -6,5 +6,11 @@ router.get('/create', (req, res) => {
     res.render('movies/create');
 });
 
+router.post('/create', (req, res) => {
+    const movieData = req.body;
+    
+    res.end()
+})
+
 
 export default router;
